acf85f8390ed52dd129e29e4caa7f00786d5f970
MUHAMAD IKHWAN <126995126+wxnshah@users.noreply.github.com>
2023-03-05 10:57:43 +0800

    Create insert.php

---
+++ insert.php
@@ -0,0 +1,28 @@
+<?php
+require_once("conn_login.php");
+include("header.php");
+
+if(isset($_POST['Submit'])) {
+	/*echo "<br>";
+	echo "<br>";
+	echo "<br>";
+	echo "<br>";
+	echo "<pre>";
+	print_r($_POST);
+	echo "</pre>";*/
+	include("assets/proses/proses_tambah.php");
+}
+
+if (isset($_SESSION['userData']) != "" && $level == 1){
+?>
+
+
+
+<?php
+} else {
+	header("Location: index.php");
+	exit(); // Quit the script.
+	include("footer.php");
+}
+include ("footer.php");
+?>
