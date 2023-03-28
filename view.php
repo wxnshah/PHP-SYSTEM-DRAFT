e781d4b83a4b6f5ce02764580bd4907960617f83
MUHAMAD IKHWAN <126995126+wxnshah@users.noreply.github.com>
2023-03-29 07:06:27 +0800

    Update view.php

--- view.php
+++ view.php
@@ -7,6 +7,34 @@ print_r($_SESSION);
 echo "</pre>";*/
 
 if(isset($_SESSION['userData']) != ""){
+	
+if(isset($_GET['success']) && $_GET['success']=="1") {
+	echo "<script>
+			setTimeout(function() {
+                Swal.fire({
+                    position: '', showConfirmButton: false, titleText: 'Simpanan Berjaya !', text: 'Permohonan anda telah berjaya dihantar !', icon: 'success', timerProgressBar: true, timer: 3000
+                })
+			}, 600);
+		</script>";
+}
+if(isset($_GET['success']) && $_GET['success']=="2") {
+	echo "<script>
+            setTimeout(function() {
+                Swal.fire({
+                    position: 'top-end', showConfirmButton: false, titleText: 'Kemaskini Berjaya !', text: 'Data anda telah berjaya dikemaskini !', icon: 'success', timerProgressBar: true, timer: 3000
+                })
+            }, 600);
+		</script>";
+}
+if(isset($_GET['success']) && $_GET['success']=="3") {
+	echo "<script>
+            setTimeout(function() {
+                Swal.fire({
+                    position: 'top-end', showConfirmButton: false, titleText: 'Padam Berjaya !', text: 'Data anda telah berjaya dipadam !', icon: 'success', timerProgressBar: true, timer: 3000
+                })
+            }, 600);
+		</script>";
+}
 ?>
 
 	<?php
