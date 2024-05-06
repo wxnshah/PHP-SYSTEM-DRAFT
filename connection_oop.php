689744f8079cc2cbf11b6a61855b94bf984024f7
MUHAMAD IKHWAN <wxn.shah@gmail.com>
2024-05-06 12:00:44 +0800

    Update connection_oop.php

--- connection_oop.php
+++ connection_oop.php
@@ -2,11 +2,6 @@
 # membuka hubungan antara laman dan pangkalan data.
 # menghantar 4 parameter asas iaitu
 # "nama host" , "username SQL" , "katalaluan SQL" , "nama pangkalan data"
-// $nama_host = "localhost";
-// $username_SQL = "root";
-// $password_SQL = "";
-// $nama_DB = "lj_homestay";
-// $condb = mysqli_connect($nama_host, $username_SQL , $password_SQL , $nama_DB);
 
 define ("HOST",'localhost');
 define ("USER",'root');
