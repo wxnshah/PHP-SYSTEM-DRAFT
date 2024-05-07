f4330da586e2786660fffdacb5c55a7a8f4a430c
MUHAMAD IKHWAN <wxn.shah@gmail.com>
2024-05-07 09:18:16 +0800

    Update register.class.php

--- assets/classes/register.class.php
+++ assets/classes/register.class.php
@@ -16,31 +16,28 @@ if (!empty($_POST))
 	}
 
 	#check data if exists
-	$result = dbquery("SELECT user_ic FROM tb_users WHERE user_ic = '".$user_ic."'");
-	// echo "SELECT user_ic FROM tb_users WHERE user_ic = '".$user_ic."'";
-	
-	if (!$result || dbrows($result) != 0){
-		// Rows exist
-		echo "<script>alert('Data Telah Didaftarkan !')</script>";
-		// redirect("register.php");
+	$result = $conn->prepare("SELECT user_ic FROM tb_users WHERE user_ic = '".$user_ic."'");
+	$stmt->execute();
+	$exists = (bool) $stmt->get_result()->fetch_row();
+
+	if ($exists) {
+		redirect("register.php?error=1");
 	} else {
 		#arahan SQL untuk menyimpan data	
 		$arahan_sql_simpan = "INSERT INTO tb_users(user_name,user_ic,user_email,password)VALUES('$user_name','$user_ic','$user_email','$password')";
 		// echo "INSERT INTO tb_users(user_name,user_ic,user_email,password)VALUES('$user_name','$user_ic','$user_email','$password')";
-		
+
 		#melaksanakan proses menyimpan data dalam syarat if
 		if(mysqli_query($conn,$arahan_sql_simpan)) {
 			#jika proses menyimpan berjaya,papar info dan buka laman login.php
-			//echo "<script>alert('Kemasukan Data Berjaya')</script>";
-			// redirect("login.php?success=1");
+			redirect("login.php?success=1");
 		}
 		else
 		{	
 			#jika proses menyimpan gagal,papar laman sebelumnya
-			echo"<script>alert('Gagal');";
-			// window.history.back();</script>";
+			echo"<script>alert('Gagal');
+			window.history.back();</script>";
 		}
-
 	}
 
 }
