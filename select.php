28871fb254f60280b35253ea85a4f3d2537ee5f3
MUHAMAD IKHWAN <wxn.shah@gmail.com>
2024-05-08 12:38:10 +0800

    Update select.php

--- select.php
+++ select.php
@@ -1,15 +1,12 @@
-
-<select class="form-control select2" name="user_dept" required autofocus>
+<select class="form-control select2" name="id_department" required autofocus>
 	<option value="">Pilih Jabatan</option>
 	<?php
-		$arahan_sql_cari = "SELECT nokp FROM tb_users";
-	 	# melaksanakan proses carian 
-	 	$laksana_arahan = mysqli_query($conn,$arahan_sql_cari);
-		while($data=dbarray($laksana_arahan)){
-		$id_jabatan = $data['id_jabatan'];
-		$nama_jabatan = $data['nama_jabatan'];
-
-			echo "<option value='".$id_jabatan."' ".($id_jabatan==$us_dept?"selected='selected'" :"").">".$nama_jabatan."</option>";
-		}
+	    $rs = dbquery("SELECT * FROM lt_department");
+	    while($data=dbarray($rs)){
+		$id_department = $data['id_department'];
+		$name_department = $data['name_department'];
+	
+		echo "<option value='".$id_department."'>".$name_department."</option>";
+	    }
 	?>
 </select>
