b57fb02672ac0bb4d348f8c5d06275bbd623e0f9
MUHAMAD IKHWAN <wxn.shah@gmail.com>
2024-05-13 10:24:31 +0800

    Update insert.class.php
    Update code for upload file (image)

--- assets/classes/insert.class.php
+++ assets/classes/insert.class.php
@@ -4,12 +4,68 @@ if (!empty($_POST))
 {
 	
 	# mengambil data POST
+	$image_student = $_FILES['image_student'];
 	$name_students = $_POST['name_students'];
 	$id_gender = $_POST['id_gender'];
 	$id_department = $_POST['id_department'];
 	$id_university = $_POST['id_university'];
 	$start_date = $_POST['start_date'];
 	$end_date = $_POST['end_date'];
+
+	$target_dir = "assets/uploads/";
+	$name = $_FILES['image_student']['name'];
+	$new_name = $target_dir.time()."-".rand(1000, 9999)."-".$name;
+	$target_file = $target_dir . basename($new_name);
+	$uploadOk = 1;
+	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+	// Check if image file is a actual image or fake image
+	if(isset($_POST["Submit"])) {
+	$check = getimagesize($_FILES["image_student"]["tmp_name"]);
+	if($check !== false) {
+		// echo "File is an image - " . $check["mime"] . ".";
+		$uploadOk = 1;
+	} else {
+		redirect('papar_borang.php?error=4');
+		// echo "File is not an image.";
+		$uploadOk = 0;
+	}
+	}
+
+	// Check if file already exists
+	if (file_exists($target_file)) {
+		// echo $target_file;
+		redirect('papar_borang.php?error=5');
+		// echo "Sorry, file already exists.";
+	$uploadOk = 0;
+	}
+
+	// Check file size
+	if ($_FILES["image_student"]["size"] > 500000) {
+		redirect('papar_borang.php?error=6');
+		// echo "Sorry, your file is too large.";
+	$uploadOk = 0;
+	}
+
+	// Allow certain file formats
+	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+		redirect('papar_borang.php?error=7');
+		// echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+	$uploadOk = 0;
+	}
+
+	// Check if $uploadOk is set to 0 by an error
+	if ($uploadOk == 0) {
+		// echo "Sorry, your file was not uploaded.";
+	// if everything is ok, try to upload file
+	} else {
+	if (move_uploaded_file($_FILES["image_student"]["tmp_name"], $target_file)) {
+		// echo "The file ". htmlspecialchars( basename( $_FILES["image_student"]["name"])). " has been uploaded.";
+	} else {
+		redirect('papar_borang.php?error=8');
+		// echo "Sorry, there was an error uploading your file.";
+	}
+	}
 		
 	#pengesahan data
 	if (empty ($name_students) || empty ($id_gender) || empty ($id_department) || empty ($id_university) || empty ($start_date) || empty($end_date))
@@ -24,14 +80,14 @@ if (!empty($_POST))
 
 	} else {
 		#arahan SQL untuk menyimpan data	
-		$arahan_sql_simpan = "INSERT INTO tb_students(name_students,id_gender,id_department,id_university,start_date,end_date)VALUES('$name_students','$id_gender','$id_department','$id_university','$start_date','$end_date')";
+		$arahan_sql_simpan = "INSERT INTO tb_students(image_student,name_students,id_gender,id_department,id_university,start_date,end_date)VALUES('$target_file','$name_students','$id_gender','$id_department','$id_university','$start_date','$end_date')";
 
 		#melaksanakan proses menyimpan data dalam syarat if
 		if(mysqli_query($conn,$arahan_sql_simpan))
 		{
 			#jika proses menyimpan berjaya,papar info dan buka laman add.php
 			//echo "<script>alert('Kemasukan Data Berjaya')</script>";
-			redirect("papar_borang.php?success=1");
+			redirect("senarai_permohonan.php?success=1");
 		}
 		else
 		{	
