cd2a1a7cb46fc6579ba5b8b4f712461347094e9e
MUHAMAD IKHWAN <wxn.shah@gmail.com>
2024-05-13 10:23:40 +0800

    Update sweetalert2.php

--- sweetalert2.php
+++ sweetalert2.php
@@ -3,27 +3,72 @@ if(isset($_GET['success']) && $_GET['success']=="1") {
 	echo "<script>
 			setTimeout(function() {
                 Swal.fire({
-                    position: 'top-end', showConfirmButton: false, titleText: 'Simpanan Berjaya !', text: 'Permohonan anda telah berjaya dihantar !', icon: 'success', timerProgressBar: true, timer: 3000
+                    position: 'top-end', showConfirmButton: false, titleText: 'Berjaya !', text: 'Data Telah Berjaya Ditambah !', icon: 'success', timerProgressBar: true, timer: 3000
                 })
 			}, 600);
 		</script>";
 }
 if(isset($_GET['success']) && $_GET['success']=="2") {
 	echo "<script>
-            setTimeout(function() {
+			setTimeout(function() {
                 Swal.fire({
-                    position: 'top-end', showConfirmButton: false, titleText: 'Kemaskini Berjaya !', text: 'Data anda telah berjaya dikemaskini !', icon: 'success', timerProgressBar: true, timer: 3000
+                    position: 'top-end', showConfirmButton: false, titleText: 'Berjaya !', text: 'Data Telah Berjaya Dikemaskini !', icon: 'success', timerProgressBar: true, timer: 3000
                 })
-            }, 600);
+			}, 600);
 		</script>";
 }
 if(isset($_GET['success']) && $_GET['success']=="3") {
 	echo "<script>
+			setTimeout(function() {
+                Swal.fire({
+                    position: 'top-end', showConfirmButton: false, titleText: 'Berjaya !', text: 'Data Telah Berjaya Dipadam !', icon: 'success', timerProgressBar: true, timer: 3000
+                })
+			}, 600);
+		</script>";
+}
+if(isset($_GET['error']) && $_GET['error']=="4") {
+	echo "<script>
             setTimeout(function() {
                 Swal.fire({
-                    position: 'top-end', showConfirmButton: false, titleText: 'Padam Berjaya !', text: 'Data anda telah berjaya dipadam !', icon: 'success', timerProgressBar: true, timer: 3000
+                    position: 'top-end', showConfirmButton: false, titleText: 'Gagal !', text: 'Fail dimasukkan bukan format image !', icon: 'error', timerProgressBar: true, timer: 3000
                 })
             }, 600);
-		</script>";
+        </script>";
+}
+if(isset($_GET['error']) && $_GET['error']=="5") {
+	echo "<script>
+            setTimeout(function() {
+                Swal.fire({
+                    position: 'top-end', showConfirmButton: false, titleText: 'Gagal !', text: 'Fail telah wujud !', icon: 'error', timerProgressBar: true, timer: 3000
+                })
+            }, 600);
+        </script>";
+}
+if(isset($_GET['error']) && $_GET['error']=="6") {
+	echo "<script>
+            setTimeout(function() {
+                Swal.fire({
+                    position: 'top-end', showConfirmButton: false, titleText: 'Gagal !', text: 'Fail dimasukkan terlalu besar !', icon: 'error', timerProgressBar: true, timer: 3000
+                })
+            }, 600);
+        </script>";
+}
+if(isset($_GET['error']) && $_GET['error']=="7") {
+	echo "<script>
+            setTimeout(function() {
+                Swal.fire({
+                    position: 'top-end', showConfirmButton: false, titleText: 'Gagal !', text: 'Format fail hanya benarkan JPG, JPEG, PNG & GIF !', icon: 'error', timerProgressBar: true, timer: 3000
+                })
+            }, 600);
+        </script>";
+}
+if(isset($_GET['error']) && $_GET['error']=="8") {
+	echo "<script>
+            setTimeout(function() {
+                Swal.fire({
+                    position: 'top-end', showConfirmButton: false, titleText: 'Gagal !', text: 'Fail gagal dimasukkan !', icon: 'error', timerProgressBar: true, timer: 3000
+                })
+            }, 600);
+        </script>";
 }
 ?>
