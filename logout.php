ec1ef32abf317cb13a3af54ed798c1ca5f837081
MUHAMAD IKHWAN <wxn.shah@gmail.com>
2024-08-01 14:39:38 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,17 +1,17 @@
 <?php
- session_start();
- 
- if (!isset($_SESSION['user'])) {
-  header("Location: index.php");
- } else if(isset($_SESSION['user'])!="") {
-  header("Location: index.php");
- }
- 
- if (isset($_GET['logout'])) {
-  unset($_SESSION['user']);
-  session_unset();
-  session_destroy();
-  header("Location: pelanggan_login.php?success=5");
-  exit;
- }
+session_start();
+
+if (!isset($_SESSION['userData'])) {
+    header("Location: index.php");
+} else if(isset($_SESSION['userData'])!="") {
+    header("Location: index.php");
+}
+
+if (isset($_GET['logout'])) {
+    unset($_SESSION['userData']);
+    session_unset();
+    session_destroy();
+    header("Location: login.php?success=5");
+    exit;
+}
 ?>
