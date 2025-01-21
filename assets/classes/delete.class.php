737913c823c35141079ce60589fcecdd69a955a1
MUHAMAD IKHWAN <wxn.shah@gmail.com>
2025-01-21 13:53:27 +0800

    Update delete.class.php | Update unlink Image & Files

--- assets/classes/delete.class.php
+++ assets/classes/delete.class.php
@@ -1,23 +1,24 @@
 <?php
-$id_students = isset($_GET['delete_id']) && $_GET['delete_id']!="" ? $_GET['delete_id'] : "";
+$id_maklumat = isset($_GET['delete_id']) && $_GET['delete_id']!="" ? $_GET['delete_id'] : "";
 
 #arahan SQL untuk padam data	
 
-// $result = dbquery("SELECT * FROM tb_students WHERE id_students = '".$id_students ."' ");
+// $result = dbquery("SELECT * FROM tb_maklumat WHERE id_maklumat = '".$id_maklumat ."' ");
 // if(dbrows($result)) {
 // 	while($data = dbarray($result)) {
-// 		unlink($data['image_student']);
+// 		unlink('assets/uploads/'.$data['image_users']);
+// 		unlink('assets/resume/'.$data['resume_users']);
 // 	}
 // }
 
-$arahan_sql_kemaskini = "DELETE FROM tb_students WHERE id_students = '".$id_students."'";
+$arahan_sql_kemaskini = "DELETE FROM tb_maklumat WHERE id_maklumat = '".$id_maklumat."'";
 	
 #melaksanakan proses menyimpan data dalam syarat if
 	if(mysqli_query($conn, $arahan_sql_kemaskini))
 	{
 		#jika proses menyimpan berjaya,papar info dan buka laman add.php
 		//echo "<script>alert('Data Berjaya Dipadam')</script>";
-		redirect("senarai_permohonan.php?success=3");
+		redirect("senarai_maklumat_pelajar.php?success=3");
 	}
 	else
 	{	
